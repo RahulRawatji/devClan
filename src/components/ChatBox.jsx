c61b89f3a979aac48c7b554cdad7212a439c6d79
rahulrawat <rahulrawatwork@gmail.com>
2023-01-29 01:32:55 +0530

    feat:

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,23 +1,23 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../utils/firebase';
-import { collection, query, where, onSnapshot, doc, deleteDoc, addDoc, serverTimestamp } from 'firebase/firestore';
+import { collection, query, where, onSnapshot, doc,  orderBy, addDoc, serverTimestamp } from 'firebase/firestore';
 
 import './ChatBox.css';
 
 const ChatBox = () => {
-  const [messageData, setMessageData] =  useState();
+  const [messageData, setMessageData] = useState();
   const [message, setMessage] = useState();
 
   const [user, loading] = useAuthState(auth);
-  
 
-  const getPost = async() => {
+
+  const getPost = async () => {
     const collectionRef = collection(db, 'messages');
-    const q = query(collectionRef);
+    const q = query(collectionRef, orderBy('createdAt', 'asc'));
     const unsubscribe = onSnapshot(q, (snapshot) => {
-      setMessageData(snapshot.docs.map((doc)=>({...doc.data(), id: doc.id})))
+      setMessageData(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
     });
 
     return unsubscribe;
@@ -28,40 +28,44 @@ const ChatBox = () => {
     getPost();
   }, []);
 
-  async function sendMessage (e) {
+  async function sendMessage(e) {
     e.preventDefault()
-    const { uid, photoURL, displayName} = auth.currentUser;
+    const { uid, photoURL, displayName } = auth.currentUser;
     console.log(auth.currentUser);
     const collectionRef = collection(db, 'messages');
-      
+
     await addDoc(collectionRef, {
-      text:message,
+      text: message,
       createdAt: serverTimestamp(),
       userId: uid,
       photoUrl: photoURL,
     })
+    setMessage('')
   }
-console.log(messageData);
+  
 
   return (
     <div className='chatbox_container'>
-        <div className='border border-b-3 p-4'>
-            <h1 className='font-bold'>Jane</h1>
-        </div>
-        <div className='chatBox_messages_container'>
-          {messageData?.map(msg=>{
-             const { uid, photoURL, displayName} = auth.currentUser;
-            const {text, userId} = msg;
-            const msgUser = userId === uid? 'sender': 'receiver';
-      
-            return <h1 className='p-2 border-b-2 m-1' style={{backgroundColor: msgUser== "sender"? 'cyan':'orange', textAlign: msgUser== "sender"? 'right':'left' }}>{text}</h1>
-          })}
-        </div>
-      
-          <form onSubmit={sendMessage}  className='flex gap-3'>
-            <input className='p-2' value={message} placeholder='Enter Your Message' style={{minWidth:"80%"}} onChange={(e)=>setMessage(e.target.value)}/>
-            <button  type="submit" className='px-5 py-3 rounded bg-slate-200'>Send</button>
-            </form>
+      <div className='border border-b-3 p-4'>
+        <h1 className='font-bold'>Jane</h1>
+      </div>
+      <div className='chatBox_messages_container'>
+        {messageData?.map(msg => {
+          const { uid } = auth.currentUser;
+          const { text, userId, photoUrl } = msg;
+          const msgUser = userId === uid ? 'sender' : 'receiver';
+
+          return (   
+            <h1 className='p-2 border-b-2 m-1 flex items-center' style={{ backgroundColor: msgUser == "sender" ? 'cyan' : 'orange', textAlign: msgUser == "sender" ? 'right' : 'left' }}> {msgUser == "receiver" ? <img  className='rounded-full m-2' style={{ border: "1px solid blue", width: "30px", marginRight: "10px"}} src={photoUrl} /> : ""}{text}</h1>
+    
+          )
+        })}
+      </div>
+
+      <form onSubmit={sendMessage} className='flex gap-3'>
+        <input className='p-2' value={message} placeholder='Enter Your Message' style={{ minWidth: "80%" }} onChange={(e) => setMessage(e.target.value)} />
+        <button type="submit" className='px-5 py-3 rounded bg-slate-200'>Send</button>
+      </form>
     </div>
   )
 }
