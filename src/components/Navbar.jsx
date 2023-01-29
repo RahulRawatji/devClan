665cdbddf1c5a19a68d2eabf6c53b00994132341
Neelesh2512 <neeleshsharma2512@gmail.com>
2023-01-29 06:34:14 +0530

    favicon change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,15 +19,16 @@ const Navbar = () => {
 
     return (
         <>
-            <a href='/home'> 
+         
             <div className='flex justify-between pt-7 px-8 pb-4 border-b-2 items-center'>
+            <a href='/home'> 
                 <div className='flex'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                     <path fillRule="evenodd" d="M2.25 6a3 3 0 013-3h13.5a3 3 0 013 3v12a3 3 0 01-3 3H5.25a3 3 0 01-3-3V6zm3.97.97a.75.75 0 011.06 0l2.25 2.25a.75.75 0 010 1.06l-2.25 2.25a.75.75 0 01-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 010-1.06zm4.28 4.28a.75.75 0 000 1.5h3a.75.75 0 000-1.5h-3z" clipRule="evenodd" />
                 </svg>
 
                     <h1 className='ml-4 text-2xl font-bold'>DevClan</h1>
                 </div>
-                </div>
+                
             </a>
             
                 {
@@ -38,6 +39,7 @@ const Navbar = () => {
                         </div>
                     )
                 }
+                </div>
         </>
     )
 }
